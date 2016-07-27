49ade479d47cac2caeb12b54f796066b7726d0c6
KQ <eternal.september@gmail.com>
2016-07-27 18:44:18 -0400

    add, create, and delete album functions work

--- music/models.py
+++ music/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.urlresolvers import reverse
+
 
 # Create your models here.
 class Album(models.Model):
@@ -6,6 +8,9 @@ class Album(models.Model):
 	title = models.CharField(max_length=500)
 	genre = models.CharField(max_length=100)
 	logo = models.CharField(max_length=1000)
+
+	def get_absolute_url(self):
+		return reverse('music:detail', kwargs={'pk': self.pk})
 
 	def __str__(self):
 		return self.artist + " - " + self.title
